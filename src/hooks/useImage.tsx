c4f61c8a031c3e680c7d1b2b31e76143a11bb4d9
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-08-12 07:06:41 -0400

    Refactor: image state to custom hook

---
+++ src/hooks/useImage.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export function useImage() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const getImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const selectedFile = e.target.files?.[0];
+      if (
+        selectedFile &&
+        (selectedFile.type === 'image/jpeg' ||
+          selectedFile.type === 'image/jpg' ||
+          selectedFile.type === 'image/png')
+      ) {
+        const imageUrl = window.URL.createObjectURL(selectedFile);
+        setSelectedImage(imageUrl);
+      } else {
+        throw new Error('Unsupported image type');
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return { selectedImage, getImage };
+}
