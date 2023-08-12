bf97eb5df0388657d5e39f583a93757f7cdee106
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-08-12 05:55:12 -0400

    Refactor: move to custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
 import CvPreview from './components/CvPreview';
 import Form from './components/Form';
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 
 import { useReactToPrint } from 'react-to-print';
+import { useImage } from './hooks/useImage';
 
 export default function App() {
+  const { getImage, selectedImage } = useImage();
   const componenRef = useRef(null);
-
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
-
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    try {
-      const selectedFile = e.target.files?.[0];
-      if (
-        selectedFile &&
-        (selectedFile.type === 'image/jpeg' ||
-          selectedFile.type === 'image/jpg' ||
-          selectedFile.type === 'image/png')
-      ) {
-        const imageUrl = window.URL.createObjectURL(selectedFile);
-        setSelectedImage(imageUrl);
-      } else {
-        throw new Error('Unsupported image type');
-      }
-    } catch (error) {
-      alert(error);
-    }
-  };
 
   const handlePrint = useReactToPrint({
     content: () => componenRef.current,
@@ -35,7 +16,7 @@ export default function App() {
   return (
     <main className="flex bg-[#0d1b2a] relative">
       <Form
-        handleImageChange={handleImageChange}
+        handleImageChange={getImage}
         handlePrint={handlePrint}
       />
       <CvPreview
